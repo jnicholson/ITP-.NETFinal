80846ae68e0d470951bd7844fd301f5b479c48ba
jnicholson <ja.nicholso@gmail.com>
2013-11-14 03:00:05 -0600

    Renaming files to Queens from WingTip

---
+++ QueensBookstore/QueensBookstore/Models/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace QueensBookstore.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext()
+            : base("QueensBookstore")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
